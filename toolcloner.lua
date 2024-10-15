e42e6f22b99dacdbf952566e2ea7e5aeaa85f3b1
vyrvall <666sixsiege@gmail.com>
2024-10-15 20:08:34 +0100

    Create toolcloner.lua

---
+++ toolcloner.lua
@@ -0,0 +1,2 @@
+_G.ToolName = "Tool"  -- Replace "Tool" with the exact name of your tool
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vyrvall/scripts/refs/heads/main/toolcloner.lua", true))()
